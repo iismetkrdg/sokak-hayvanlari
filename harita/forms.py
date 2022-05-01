fecb39027953e171dff3163c82430314db918107
İsmet Karadağ <ismet.k3908@gmail.com>
2022-05-01 17:27:22 +0300

    cbw

--- harita/forms.py
+++ harita/forms.py
@@ -7,14 +7,16 @@ from .models import Kulube
 class YuvaBildirForms(forms.ModelForm):
    class Meta:
       model=Kulube
-      fields=['il','ilce','mahalle','sokak','kapı','acıklama']
+      fields=['il','ilce','mahalle','sokak','kapi','aciklama']
 
    il = forms.CharField(label='İl',max_length=50)
    ilce = forms.CharField(label='İlçe',max_length=100)
    mahalle = forms.CharField(label='Mahalle',max_length=100)
    sokak = forms.CharField(label='Sokak', max_length=100)
-   kapı = forms.CharField(label='Kapı No', max_length=50)
-   acıklama = forms.CharField(label='Açıklama', max_length=500)
+   kapi = forms.CharField(label='Kapı No', max_length=50)
+   aciklama = forms.CharField(label='Açıklama', max_length=500,required=False)
+   dogcat = forms.CharField(label='dogcat',max_length=5)
+   
 
 class BesleForms(forms.Form):
    mama = forms.CharField(label='kackilo',max_length=50)
