f5b388c10624573c0b80a0e290fc3133986436dc
İsmet Karadağ <ismet.k3908@gmail.com>
2022-05-24 18:13:37 +0300

    try

--- sokaktakiler/settings.py
+++ sokaktakiler/settings.py
@@ -137,9 +137,9 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS=(
+STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static") 
-)
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
