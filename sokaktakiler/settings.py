7997792d9152f2b1ed8027969b48eb72f7f39931
İsmet Karadağ <ismet.k3908@gmail.com>
2022-05-24 19:46:07 +0300

    ks

--- sokaktakiler/settings.py
+++ sokaktakiler/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE  =  'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # Default primary key field type
