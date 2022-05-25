d2e682c7f4cd3e7d8647c0a5bb28b7e040c45f11
İsmet Karadağ <ismet.k3908@gmail.com>
2022-05-25 20:36:21 +0300

    2

--- sokaktakiler/settings.py
+++ sokaktakiler/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG',False)
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['agile-basin-70896.herokuapp.com']
 
