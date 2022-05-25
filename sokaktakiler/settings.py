577a24d3dec66a8d288b922e8f7054c26b15183e
İsmet Karadağ <ismet.k3908@gmail.com>
2022-05-25 20:18:00 +0300

    finish commit

--- sokaktakiler/settings.py
+++ sokaktakiler/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s6lvihxk^)(gx=bk=6*61$s8t^7pflpvirm=*wua7#afws8=@z'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('IS_DEVELOPPEMENT',False)
 
 ALLOWED_HOSTS = ['agile-basin-70896.herokuapp.com']
 
@@ -148,7 +148,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # email 
-SENDGRID_API_KEY = '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'
+SENDGRID_API_KEY = os.getenv('SENGRID_API_KEY')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_HOST_USER = 'apikey'
